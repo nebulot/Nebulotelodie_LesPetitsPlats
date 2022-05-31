36c212cad2deb8467a440d7b9e2d21da9aa29153
nebulot elodie <nebelodie@gmail.com>
2022-05-31 11:34:02 +0200

    html bootstrap

--- scripts/index.js
+++ scripts/index.js
@@ -7,24 +7,33 @@ console.log(await getRecipes);
 //display cards recipes
  
 export class RecipeCard {
-  constructor(recipe, indexCard) {
+  constructor(recipe) {
   this.recipe = recipe;
-  this.indexCard = indexCard;
+  
   }
   
   createHtml() {
     return ` 
     <article class="card">
-    <img class="photographer-medium_gallery" alt="${this.title}" src= "./assets/images/${this.photographerId}/${this.image}"/></a>
-    <footer class="cards-media-footer">
-    <p class="cards-media_title">${this.title}</p>
-    <div class="cards-media_likes">
-    <span class="cards-media_total_likes" title="J'aime">${this.likes}</span>
-    <bouton class="cards-media_total_likes_btn" role="button" aria-label="ajouter un j'aime">
-    <i class="far fa-heart" aria-hidden="true" aria-label="likes"></i>
-    </button>
-    </div>
-    </footer>
-    </div> `;
+    <a href="#">
+    <div class="card_img">
+        <div class="card_recipe">
+        <h2 class="card_title">title</h2>
+        <div class="card_time">
+        <i class="card_oclock"></i>
+        <p class="card_min">minute min</p>
+        </div>
+        </div>
+        <div class="card_content">
+        <ul class="card_ingredients">
+                        listCard_HTML
+                        </ul>
+                        <p class="card_description">
+                        description
+                        </p>
+                      </div>
+                      </div>
+                  </a>
+                </article>`
   }
 }
