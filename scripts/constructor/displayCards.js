2ab5242076d6b1f5ffc8d02694425437ec0041a7
nebulot elodie <nebelodie@gmail.com>
2022-06-02 19:04:06 +0200

    card display

--- scripts/constructor/displayCards.js
+++ scripts/constructor/displayCards.js
@@ -1,46 +1,43 @@
-
-// create DOM recipe CARD 
+// create DOM recipe CARD
 
 export class RecipeCard {
   constructor(data) {
-    
-	this._id = data.id;
-	this._name = data.name;
-	this._description = data.description;
-	this._time = data.time;
-	this._servings = data.servings;
-	this._ustensils = data.ustensils;
-	this._ingredients = data.ingredients;
-	this._appliance = data.appliance;
-	}
-  
+    this._id = data.id;
+    this._name = data.name;
+    this._description = data.description;
+    this._time = data.time;
+    this._servings = data.servings;
+    this._ustensils = data.ustensils;
+    this._ingredients = data.ingredients;
+    this._appliance = data.appliance;
+  }
 
   get recipeCard() {
-	  document.querySelector(".receipe-container").insertAdjacentHTML(
-		  "afterbegin",
-     `  <article class="receipe_card">
-		<a href="#">
-		<div class="card_img">
-			<div class="card_recipe">
-			<h2 class="card_title">${this._name}</h2>
-			<div class="card_time">
-			<i class="card_oclock"></i>
-			<p class="card_min">${this._time}min</p>
-			</div>
-			</div>
-			<div class="card_content">
-			<ul class="card_ingredients">
-							listCard_HTML
-							</ul>
-							<p class="card_description">
-							${this._description}
-							</p>
-						  </div>
-						  </div>
-					  </a>
-					</article>`
-					
-	);
+	  const recipeCard = document.createElement("div");
+	  recipeCard.className = "recipeCard col-12 col-md-6 col-lg-4";
+    document.querySelector(".receipe-container").insertAdjacentHTML(
+      "afterbegin",
+recipeCard.innerHTML = 
+      ` <div class="card rounded border-0">
+	    <div class="card-header bg-secondary"></div>
+	    <div class="card-body bg-light">
+	    <div class="title-time">
+		 <h5 class="card-title m-0 font-weight-light">
+		   ${this._name}
+		 </h5>
+		 <div class="recipe_time">
+		 <i class="far fa-clock"></i>
+		 <h5 class="time">${this._time}min</h5>
+	   </div>
+	 </div>
+	 <div class="ingredients-directions row">
+	   <ul class="recipe-ingredient-list list-group list-unstyled bg-light">
+	   </ul>
+	   <p class="card-text">
+	   ${this._description}
+	   </p>
+	 </div>
+   </div>`
+    );
   }
 }
-
