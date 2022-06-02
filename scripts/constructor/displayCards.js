a3da3d6832da133ffee0378a6e799d295b71f645
nebulot elodie <nebelodie@gmail.com>
2022-06-02 22:23:24 +0200

    ingredient liste

--- scripts/constructor/displayCards.js
+++ scripts/constructor/displayCards.js
@@ -10,34 +10,51 @@ export class RecipeCard {
     this._ustensils = data.ustensils;
     this._ingredients = data.ingredients;
     this._appliance = data.appliance;
+    console.log(data);
   }
 
   get recipeCard() {
-	  const recipeCard = document.createElement("div");
-	  recipeCard.className = "recipeCard col-12 col-md-6 col-lg-4";
+    const recipeContainer = document.createElement("div");
+    recipeContainer.className = "recipeCard col-12 col-md-6 col-lg-4";
     document.querySelector(".receipe-container").insertAdjacentHTML(
       "afterbegin",
-recipeCard.innerHTML = 
-      ` <div class="card rounded border-0">
+      recipeContainer.innerHTML = ` 
+	  <div class="card rounded border-0">
 	    <div class="card-header bg-secondary"></div>
 	    <div class="card-body bg-light">
 	    <div class="title-time">
-		 <h5 class="card-title m-0 font-weight-light">
+		 <h2 class="card-title m-0 font-weight-light">
 		   ${this._name}
-		 </h5>
+		 </h2>
 		 <div class="recipe_time">
 		 <i class="far fa-clock"></i>
-		 <h5 class="time">${this._time}min</h5>
+		 <h3 class="time">${this._time}min</h3>
 	   </div>
 	 </div>
 	 <div class="ingredients-directions row">
-	   <ul class="recipe-ingredient-list list-group list-unstyled bg-light">
+	   <ul class="ingredient-list list-group list-unstyled bg-light">
 	   </ul>
 	   <p class="card-text">
 	   ${this._description}
 	   </p>
 	 </div>
+   </div>
+   </div>
    </div>`
     );
+	this.ingredientsCard();
   }
+
+// create li to put ingredients[array].ingredients.ingredients
+get ingredientsCard() {
+	const ingredients = recipeCard.querySelector('.recipe-ingredient-list');
+    const ingredientsList = recipe.ingredients.map(ingredient => {
+		if(ingredient.quantity) {
+return
+`<li class = "ingredient-list">${ingredient.ingredient} : ${ingredient.quantity ?? ''} ${ingredient.unit ?? ''}</li>`
+} else {
+	`<li class = "ingredient-list">${ingredient.ingredient}`	
+}
+});
+}
 }
