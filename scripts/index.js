390f9069cfb7f2113535d114d5d31374d5d21777
nebulot elodie <nebelodie@gmail.com>
2022-07-15 12:00:50 +0200

    commit change

--- scripts/index.js
+++ scripts/index.js
@@ -1,130 +1,64 @@
-///////////////////////5 DOM for "searchbar" and 8 "tags"/////////////////////////////
+//////////////////////5 DOM for "searchbar" and 8 "tags"/////////////////////////////
 const dropdownsBar = document.querySelector(".btns-dropdown");
 const tags = document.querySelector(".tags");
 
-//3 container card recipes
-//display cards recipes
-/*const recipesSection = document.querySelector(".receipe-container");
-const displayCards = (recipes) => {
-  recipes.forEach((recipe) => {
-    //console.log(recipe); ok
-    recipesSection.append(new RecipeCard(recipe).createRecipeCard);
-    console.log(RecipeCard);
-  console.log(createRecipeCard); ok
-
-  });
-};*/
-
 //////////// 9 create an alert display under searchbar to success or danger research //////
-/*export 
-  };*/
+let globalRecipesList = recipes.slice();
+console.log(recipes); //=> searchBar arrayalgo.js
 
+// research array from all btns dropdownfilters
+//=> function on click() check dropdownsBar.js
+const initFilters = (recipes) => {
+	let ingredients = [];
+	let appareil = [];
+	let ustensiles = [];
+  //console.log(ingredients); ok []
+  //console.log(appareil); ok []
+
+recipes.forEach((recipe) => {
+  ingredients = [
+          // chercher un tri doublons, majuscules,...
+    ...new Set([...ingredients, ...recipe.ingredients.map((i) => i.ingredient)])].sort();
+  ustensiles = [...new Set([...ustensiles, ...recipe.ustensils.map((u) => u)])].sort();
+  appareil = [...new Set([...appareil, ...[recipe.appliance]])].sort();
+  //console.log(ingredients); ok {'coco' 'noix de coco' .....}/recette
+  //console.log(appareil); ok {'four','blender '}/recette
+});
+return { ingredients, appareil, ustensiles };
+}
 
 ////////////////////////////////INIT////////////////////////////////
 //error stop async 
-function init() {
-  
-  /*const { recipes } = await getRecipes();
-  displayCards();
-  //console.log(recipes);   //=> search arrayalgo.js*/
-  
-  // return bts'dropdown
-  const dropdownsValues = ['ingredients', 'appareils', 'ustensiles'];
-    for (let type of dropdownsValues) {
-        const dropdownsBtn = new DropdownsBuilder(type);
-        dropdownsBtn.make().forEach(btn => dropdownsBar.append(btn));
-        //console.log(dropdownsBtn); (type : "", color :"");
-        //console.log(dropdownsBar); <div "btns-dropdown">
-    }
-    // console.log(dropdownsValues); results (3)[ingredients,appliances,ustensils]
-
-// 8 alert tag searchbar 
+/*function init() {
+7 dropdown => dropdown.js
+8 tag searchbar => tags.js
 const tag = new Tag('ingredients', 'lait de coco').make();
-    tags.appendChild(tag);
+    tagsBox.appendChild(tag);
     const tag2 = new Tag('appareils', 'four').make();
-    tags.appendChild(tag2);
+    tagsBox.appendChild(tag2);
     const tag3 = new Tag ('ustensiles', 'fouet').make();
-    tags.appendChild(tag3); 
+    tagsBox.appendChild(tag3); 
 };
-init();
+init();*/
 
 ///////////////////////    (5) SEARCHBAR  arrayAlgo.js  ////////////////////////////////
-const search = document.querySelector("form .input-group ");
-const results = document.querySelector("main .receipe-container");
-
+const main = document.querySelector(".main");
+const search = document.querySelector(".input-group ");
+const results = document.querySelector(".results .receipe-container");
 const searchBarInput = search.querySelector("#search-bar");
 const submit = search.querySelector("span");
 
-//8 alert display 
-const displayAlert = (count) => {
-  const alerts = document.querySelector(".alerts");
-  alerts.append(new Alerts(count).handleAlert());
-}
+
 //5 focus on searchbar and check all recipes : nodeList(50) Alert
-searchBarInput.addEventListener("keyup", (e) => {
-  //const searchRecipes = e.target.value;
-  const cards = document.querySelectorAll(".card");
-  let count = filteredCards(searchRecipes, cards);
-  displayAlert(count);
-  console.log(searchRecipes);
-});
-
-
+//+>searchBar.js
 
 //////////////////////     (7)   DROPDOWN ////////////////////////////
-const appareils = dropdownsBar.querySelectorAll(".ingredients");
-const ustensils = dropdownsBar.querySelectorAll(".ustensiles");
-const appliances = dropdownsBar.querySelectorAll(".appareils");
-
-const open = dropdownsBar.querySelectorAll("#active");
-const hidden = dropdownsBar.querySelectorAll("#inactive");
-
-
-
 window.addEventListener('load', initRecipes);   //ask computer to querylength ?
 searchBarInput.addEventListener('input', searchRecipes);   //searchBar.js function
 search.addEventListener('submit', (e) => e.preventDefault()); 
 
 ////////////////////////// TAGS BTN ////////////////////////////////////
-//global 
-  function closeTags(e) {
-    const tagsBtn = [...tags.querySelectorAll("button")];
-    const [container] = tagsBtn.filter(btn => btn.contains(e.target));
-    container.remove();
-    //console.log(tagsBtn); ok
-  } 
-
-/////////////////////////////// event DROPDOWN global ////////////////////////////////
-  
-hidden.forEach(btn => btn.addEventListener('click', displayDropdown));
-open.forEach(btn => btn.querySelector('button').addEventListener('click', displayDropdown));
-console.log(hidden); //nodeList inactive (3) ok
-console.log(open); //nodeList active (3) ok
-
-function displayDropdown(e) {  
- // all btns is inactive we don't see the {typeList}
- // use el for element with (display, filter, contains....)
-
-  [...open].forEach(el => el.style.display = "none");
-  [...hidden].forEach(el => el.style.display = "flex");
-
-  // when you click on the  btns, one btn is active and you see {typeList}
-  const [container] = [...open].filter(el => el.contains(e.target)).length > 0
-      ? [...open].filter(el => el.contains(e.target))
-      : [...hidden].filter(el => el.contains(e.target));
-  console.log(container);
-
-  const isOpen = container.id;
-  //console.log(container); //div btn-{type}-container ok
-  //console.log(isOpen); //inactive div ok
-  const type = container.classList[container.classList.length - 1];
-  const [siblingContainer] = [...dropdownsBar.querySelectorAll(`.${type}`)].filter(el => el.id != isOpen);
-  //console.log(isOpen); inactive to
-  container.style.display = "flex" ? "none" : "flex";
-  siblingContainer.style.display = container.style.display == "flex" ? "none" : "flex";
-  console.log(siblingContainer);
-  //siblingContainer ; div wrapper-{type} input-dropdown + chevron 
-}
+//global   
   
 
 /////////////   RECIPE  RESULT CARD //////////////////////
@@ -133,5 +67,7 @@ function displayDropdown(e) {
 function displayResults() {
   for (let recipe of searchResult) {
       results.appendChild(recipe);
+      
   }
-}
+  // displayResults give the number results of the research on the search bar
+};
---
+++ scripts/utils/dropdown.js
@@ -0,0 +1,152 @@
+//text create with [active] and [inactive] problem 
+
+const selectedTags = [];
+//use second solution dom Element and function onclick(); 
+const dropdownValues = (recipes) => {
+  const { ingredients, appareil, ustensiles } = initFilters(recipes);
+};
+
+
+//////////DOM ELEMENTS/////////////////////////////
+// close and open dropdown 
+const btn1 = document.getElementById("btn-ingredients");
+const btn2 = document.getElementById("btn-appareils");
+const btn3 = document.getElementById("btn-ustensiles");
+
+const appareilInput = document.querySelector(".appareil_input");
+const ustensilsInput = document.querySelector(".ustensils_input");
+
+const li = document.getElementsByClassName("result");
+
+const ingredientIcon = document.getElementsByClassName("ingredients_arrow");
+const appareilsIcon = document.getElementsByClassName("appliance_arrow");
+const ustensilsIcon = document.getElementsByClassName("ustensils_arrow");
+
+//main.addEventListener("click", closeAll);
+//search.addEventListener("click", closeAll);
+
+btn1.onclick = function() {
+  let contentBtn = document.getElementById("links-ingredients");
+  //let ingredientsIcon = document.getElementsByClassName("ingredients_arrow");
+  //shrink the button
+  if (contentBtn.style.display !== "none") {
+    contentBtn.style.display = "none";
+    
+}else{
+  contentBtn.style.display = "block";
+      //expand the button
+  //ingredientsIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
+  const tags = initFilters(recipes);
+  console.log(tags);
+  tags.ingredients.forEach((ingredient) => {
+    const ul = document.querySelector("#links-ingredients .result");
+    const li = document.createElement("li");
+    li.textContent = ingredient;
+    li.className = "ingredient_item";
+    ul.appendChild(li);
+    //console.log(li);
+    //console.log(ul);
+})};
+listIngredientsItems();
+};
+
+const ingredientsInput = document.querySelector(".ingredient_input");
+ingredientsInput.addEventListener("keyup", (e) => {
+const tags = initFilters(recipes);
+//const ul = document.querySelector("#links-ingredients .result");
+  //ul.innerHtml ="";
+  const query = e.target.value.toLowerCase();
+  console.log(query); //ok scibe pomm....
+  const searchresults = tags.ingredients.filter((ingredient) => {
+				return ingredient.toLowerCase().includes(query);
+			}); 
+      searchresults.forEach((result) => {
+        const searchresults = document.createElement("div");
+        results.innerHtml = result ;
+        });
+        console.log(searchresults);
+        listIngredientsItems();
+			});
+     
+  const listIngredientsItems = () => {
+  const li = document.querySelectorAll(".ingredient_item");
+  li.forEach((item) => {
+    item.addEventListener("click", () => {
+      selectedTags.push(item.textContent);
+      const selectedTagsStop = [...new Set(selectedTags)];
+      createTagsBar(selectedTagsStop, recipes);
+      //console.log(createTagsBar);
+    });
+  });
+};
+		
+////////////////////////////////BUTTON 2 APPAREIL//////////////////////////////////
+
+btn2.onclick = function() {
+  let contentBtn2 = document.getElementById("links-appareils");
+ 
+  //shrink the button
+  if (contentBtn2.style.display !== "none") {
+    contentBtn2.style.display = "none";
+    
+}else{
+  contentBtn2.style.display = "block";
+  //expand the button
+  const tags = initFilters(recipes);
+  tags.appareil.forEach((appliance) => {
+    const ul = document.querySelector("#links-appareils .result");
+    const li = document.createElement("li");
+    li.textContent = appliance;
+    li.className = "appareil_item";
+    ul.appendChild(li);
+  })};
+  listAppareilsItems();
+}
+
+const listAppareilsItems = () => {
+  const appareilsItems = document.querySelectorAll(".appareil_item");
+  appareilsItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      selectedTags.push(item.textContent);
+      const selectedTagStop = [...new Set(selectedTags)];
+      createTagsBar(selectedTagStop, recipes);
+    });
+  });
+};
+
+///////////////////////////////////////BOUTON 3 USTENSILES/////////////////////////////////////////
+
+btn3.onclick = function() {
+  let contentBtn3 = document.getElementById("links-ustensiles");
+  
+  //shrink the button
+  if (contentBtn3.style.display !== "none") {
+    contentBtn3.style.display = "none";
+    
+}else{
+  contentBtn3.style.display = "block";
+      //expand the button
+      const tags = initFilters(recipes);
+  tags.ustensiles.forEach((ustensils) => {
+    const ul = document.querySelector("#links-ustensiles .result");
+    const li = document.createElement("li");
+    li.textContent = ustensils;
+    li.className = "ustensils_item";
+    ul.appendChild(li);
+  })};
+  listUstensilesItems();
+}
+
+const listUstensilesItems = () => {
+  const ustensilesItems = document.querySelectorAll(".ustensils_item");
+  console.log(ustensilesItems); //nodeList (30) li.ustensils_items
+  ustensilesItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      selectedTags.push(item.textContent);
+      console.log(selectedTags);// number of selection all (3)
+      const selectedTagsStop = [...new Set(selectedTags)];
+      createTagsBar(selectedTagsStop, recipes);
+      
+    });
+  });
+};
