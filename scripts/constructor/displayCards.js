90d6960d42e2800012e177d68a9587b8df8958bd
Torkneb <87141819+nebulot@users.noreply.github.com>
2025-03-15 17:53:24 +0100

    Update displayCards.js
    console test

--- scripts/constructor/displayCards.js
+++ scripts/constructor/displayCards.js
@@ -65,6 +65,8 @@ class RecipeCard {
   make() {
     const card = document.createElement("div");
     card.className = "receipe col-12 col-md-6 col-lg-4";
+	  console.log("Image URL:", `assets/recettes/${this._image}`);
+
     card.innerHTML = `
     <div class="card rounded border-0">
     <div class="card-header bg-grey">
